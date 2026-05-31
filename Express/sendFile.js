edc35acfe6b7f3021222a40bca5772af1c14c9b3
Ritik811 <ritikkumar32855@gmail.com>
2026-05-31 21:43:25 +0530

    Send File Complete

---
+++ Express/sendFile.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { PORT } from "./env.js";
+import path from "path";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  //   console.log(import.meta.dirname);
+  //   console.log(import.meta.filename);
+  //   const __fileName = import.meta.dirname;
+  //   console.log(__fileName);
+  const homePath = path.join(import.meta.dirname, "public", "index.html");
+  return res.sendFile(homePath);
+});
+
+app.listen(PORT, () => {
+  console.log("Server is Running PORT: ", PORT);
+});
