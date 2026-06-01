1cfd6e30129b0992a776e1c8d8b953dc466cd05a
Ritik811 <ritikkumar32855@gmail.com>
2026-06-01 13:31:22 +0530

    Params Complete

--- Express/app.js
+++ Express/app.js
@@ -7,6 +7,18 @@ app.get("/", (req, res) => {
   res.send("hello World");
 });
 
+// Params
+
+app.get("/:username/article/:title", (req, res) => {
+  console.log("params is ", req.params);
+  const formatTitle = req.params.title.replaceAll("-", " ");
+  res.send(
+    `<h1>Hello Bro Welcome to My Website ${req.params.username} and article is ${formatTitle} </h1>`,
+  );
+});
+
+
+
 app.listen(PORT, () => {
   console.log(`Server is Running at PORT:  ${PORT}`);
 });
