228723a2e95f4f40f561286774cc4db54f4e76f7
Ritik811 <ritikkumar32855@gmail.com>
2026-06-02 15:55:32 +0530

    GET and POST req complete

--- Express/RestApi/app.js
+++ Express/RestApi/app.js
@@ -9,8 +9,33 @@ app.use(express.static(path.join(import.meta.dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Start Project Rest API");
+let posts = [
+  {
+    username: "Ritik Rajput",
+    content: "I Love Coding",
+  },
+  {
+    username: "Sonia",
+    content: "I Love Not Coding",
+  },
+  {
+    username: "Vishal Billa",
+    content: "I Love Traveling",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.render("index", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("new");
+});
+
+app.post("/posts/new", (req, res) => {
+  const { username, content } = req.body;
+  posts.push({ username, content });
+  res.redirect("/posts");
 });
 
 const PORT = 8080;
