711d17a1b3747ec74e49f34a356f6dd3941918c6
Ritik811 <ritikkumar32855@gmail.com>
2026-06-04 00:11:22 +0530

    Basic Setup Express and Mongoose

---
+++ Express/ExpressWithMongodb/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = 8080;
+
+const connectDataBase = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/chatDB");
+    console.log("dataBase Connected Successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connectDataBase();
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(import.meta.dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Working");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is Running PORT", PORT);
+});
